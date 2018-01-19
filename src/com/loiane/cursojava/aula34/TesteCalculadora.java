23cb17322fea192c36cfe5d43b9c68d9661a5ff3
Thiago Alves da Costa <thiago.alves.ec@gmail.com>
2018-01-18 23:16:46 -0200

    atualizar

--- src/com/loiane/cursojava/aula34/TesteCalculadora.java
+++ src/com/loiane/cursojava/aula34/TesteCalculadora.java
@@ -11,11 +11,20 @@ package com.loiane.cursojava.aula34;
  */
 public class TesteCalculadora {
     
+    static int resultSoma;
     public static void main(String[] args){
         
-        MinhaCalculadora cal = new MinhaCalculadora();
+        resultSoma = MinhaCalculadora.soma(1, 2);
+        //MinhaCalculadora cal = new MinhaCalculadora();
         
-        cal.soma(1, 2);
+        //.soma(1, 2);
+        
+        soma2Valores(1, 2);
+        
+    }
+    
+    static int soma2Valores(int num1, int num2){
+        return MinhaCalculadora.soma(num1, num2);
     }
     
 }
